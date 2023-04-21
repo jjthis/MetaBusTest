7d16e6414e26c4414caf5e55c213c6972f63d179
jthis <aasgh443@gmail.com>
2023-04-22 05:50:58 +0900

    'ad'

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
+
 app.listen(8080, function () {
-    console.log('listening on 8080');
+    console.log('listening on 8080')
 });
 app.use(express.static(path.join(__dirname, 'react_project/build')));
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/react-project/build/index.html'));
 });
